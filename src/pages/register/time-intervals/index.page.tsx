8b6287d269d1fa4270d6faa101c68aca7b3d3f85
Nicolas Segatto <nicolasssegatto@icloud.com>
2023-10-04 15:13:32 -0300

    finalizacao do cadastro

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import {
   IntervalsContainer,
   FormError,
 } from './styles'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -86,6 +87,7 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
   const weekDays = getWeekDays()
 
   const { fields } = useFieldArray({
@@ -100,6 +102,8 @@ export default function TimeIntervals() {
     await api.post('/users/time-intervals', {
       intervals,
     })
+
+    await router.push('/register/update-profile')
   }
 
   return (
