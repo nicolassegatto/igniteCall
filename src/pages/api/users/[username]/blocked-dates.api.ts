4cd3d1492526439e0c456b409b910487cc59773a
Nicolas Segatto <nicolasssegatto@icloud.com>
2023-11-28 20:38:42 -0300

    blockedDays

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,7 +1,6 @@
 import { prisma } from '@/lib/prisma'
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { date } from 'zod'
 
 export default async function handle(
   req: NextApiRequest,
@@ -28,5 +27,20 @@ export default async function handle(
     return res.status(400).json({ message: 'User doesnt exist.' })
   }
 
-  return res.json({})
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+    return !availableWeekDays.some(
+      (availableWeekDay) => availableWeekDay.week_day === weekDay,
+    )
+  })
+
+  return res.json({ blockedWeekDays })
 }
